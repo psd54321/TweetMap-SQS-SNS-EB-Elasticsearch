3f1cf3cba0770bd70b813fb3efbca0a819948450
prathameshdinkar <psd54321@gmail.com>
2016-11-20 22:35:49 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -57,7 +57,7 @@ router.post('/notify', function (req, res) {
     } else if(req.get('x-amz-sns-message-type') == 'SubscriptionConfirmation') {
         console.log('inside subscription');
         var subscribeURL = req.body.SubscribeURL;
-        console.log(req.params);
+        console.log(req.get('Content-Length'));
         console.log('after url');
         https.get(subscribeURL, function(res) {
             console.log('Subscription Confirmed!');
