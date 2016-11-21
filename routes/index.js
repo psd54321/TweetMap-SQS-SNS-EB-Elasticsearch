5ce07b73a0aa570c0014b574610391726b2771ef
prathameshdinkar <psd54321@gmail.com>
2016-11-20 19:59:31 -0500

    socket sns sqs notify sns

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,7 @@ router.get('/search/:searchq', function (req, res) {
 
 router.post('/notify', function (req, res) {
     io =res.io;
-     io.sockets.emit('tweet','message sent notify before if');
+     io.sockets.emit('tweet','message sent notify before if '+req.get('x-amz-sns-message-type'));
     if(req.get('x-amz-sns-message-type') == 'Notification') {
         var tweet = JSON.parse(JSON.parse(req.body).Message).text;
         // extract sentiment info from DB
