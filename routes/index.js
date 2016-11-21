425e3e417dafb0722b1c8d24d20c545a9e1f69b0
prathameshdinkar <psd54321@gmail.com>
2016-11-20 20:20:56 -0500

    socket sns sqs notify sns

--- routes/index.js
+++ routes/index.js
@@ -50,8 +50,8 @@ router.get('/search/:searchq', function (req, res) {
 
 router.post('/notify', function (req, res) {
     io =res.io;
-     io.sockets.emit('tweet','message sent notify before if '+req.get('x-amz-sns-message-type'));
-      io.sockets.emit('tweet','message sent notify before if '+JSON.parse(req.body).SubscribeURL);
+     //io.sockets.emit('tweet','message sent notify before if '+req.get('x-amz-sns-message-type'));
+      //io.sockets.emit('tweet','message sent notify before if '+JSON.parse(req.body).SubscribeURL);
      validator.validate(JSON.parse(req.body),function (err,message){
          io.sockets.emit('tweet','message sent notify inside');
         if (err) {
